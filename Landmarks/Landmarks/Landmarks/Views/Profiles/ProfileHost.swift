57c5907e61b92ae83ae7532ace9cddc9718bf711
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-18 17:07:00 +0100

    Toggle edit mode

--- Landmarks/Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @Environment(ModelData.self) var modelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
-        Text("Profile for \(draftProfile.username)")
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
+            if (editMode?.wrappedValue == .inactive) {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
+
+        }
+        .padding()
+        
+        
     }
 }
 
 #Preview {
     ProfileHost()
+        .environment(ModelData())
 }
