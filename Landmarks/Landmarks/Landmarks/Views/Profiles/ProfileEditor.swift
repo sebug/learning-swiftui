b0d54f84b65a2732f4daf4de5d9632139e05e856
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-18 17:16:53 +0100

    Native controls

--- Landmarks/Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
+++ Landmarks/Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct ProfileEditor: View {
     @Binding var profile: Profile
+    var dateRange: ClosedRange<Date> {
+           let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+           let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+           return min...max
+       }
     
     var body: some View {
         List {
@@ -18,6 +23,19 @@ struct ProfileEditor: View {
                 TextField("Username", text: $profile.username)
                     .foregroundStyle(.secondary)
                     .multilineTextAlignment(.trailing)
+            }
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notifications")
+            }
+            Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                ForEach(Profile.Season.allCases) { season in
+                    Text(season.rawValue).tag(season)
+                }
+            }
+            DatePicker(selection: $profile.goalDate,
+                       in: dateRange,
+                       displayedComponents: .date) {
+                Text("Goal Date")
             }
         }
 
