40eeb2f8142eae8b77453c753c926da5471c1a7d
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-18 12:24:35 +0100

    Landmark row

---
+++ Landmarks/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Sebastian Gfeller on 18.12.2023.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
