6c162af8abaea62f3d5d421471587d1a6446fab8
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-18 16:59:57 +0100

    Profile summary

---
+++ Landmarks/Landmarks/Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Sebastian Gfeller on 18.12.2023.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+
+
+                Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileSummary(profile: Profile.default)
+}
