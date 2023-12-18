edb27aff4b4fb30b8066627a4fda87b8856e367e
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-18 12:43:17 +0100

    Navigation Link

---
+++ Landmarks/Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,44 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Sebastian Gfeller on 18.12.2023.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+            }.padding()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
