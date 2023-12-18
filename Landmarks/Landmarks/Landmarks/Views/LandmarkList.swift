c9aa4581c30404bd368b0932ac920a9776597e32
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-18 12:31:50 +0100

    List with items

---
+++ Landmarks/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Sebastian Gfeller on 18.12.2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
