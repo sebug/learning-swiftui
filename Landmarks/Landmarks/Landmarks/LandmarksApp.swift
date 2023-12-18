bce68159f4055aef1dde4c49de6bb8fedab72f82
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-18 10:49:09 +0100

    Project basics

---
+++ Landmarks/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sebastian Gfeller on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
