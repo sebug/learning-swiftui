e108c572176fe121622385266f15814874be46fd
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-12-18 11:32:13 +0100

    Adding circle image

---
+++ Landmarks/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Sebastian Gfeller on 18.12.2023.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
